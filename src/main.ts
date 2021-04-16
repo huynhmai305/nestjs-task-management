5b79fb86c00e3ded9e641d9afb95824eb1322f89
Huynh Mai <huynhmai305@gmail.com>
2021-04-16 13:28:35 +0700

    Update server

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,11 @@ async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-
   const port = process.env.PORT || serverConfig.port;
+
+  if (process.env.NODE_ENV) {
+    app.enableCors();
+  }
 
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
